80a1670bcacab41546fcada9da89808289a58c73
JaxenTSandland <jsandland@student.neumont.edu>
2025-04-28 18:05:28 -0600

    Changed around env variable names

--- src/app.js
+++ src/app.js
@@ -32,10 +32,10 @@ app.use(cors());
 app.use(express.json());
 
 const sqlPool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQL_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
